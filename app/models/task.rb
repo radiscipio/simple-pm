d32dd0a4be36a9fc65bb24f4fd972bfb64420691
radiscipio <robertdiscipio@gmail.com>
2020-12-20 17:25:11 -0500

    Added select progress to form

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,10 @@ class Task < ApplicationRecord
   belongs_to :project
 
   validates :status, inclusion: { in: ['not-started', 'in-progresss', 'complete'] }
+
+  STATUS_OPTIONS = [
+    ['Not Started', 'not-started'],
+    ['In Progress', 'in-rogress'],
+    ['Complete', 'complete']
+  ]
 end
